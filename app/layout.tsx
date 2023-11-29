031a0c41d3e5718ea0b0220fc128eb1345af02eb
sellerscrisp <sellershcrisp@gmail.com>
2023-11-29 15:00:09 -0500

    📊🧪 Fixed comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </div>
         </ThemeProvider>
         <Analytics />
-        // meticulous recorder script
+        {/* meticulous recorder script */}
         <script data-project-id="pX9lSO92h5Tzp5h7WwrcH8vAsVygzVXPMzC0oQDI" src="https://snippet.meticulous.ai/v1/meticulous.js"/>
       </body>
     </html>
