41b61d29ec25cbd4ff2274bd11333b92c8b1010f
sellerscrisp <sellershcrisp@gmail.com>
2023-12-12 13:54:02 -0500

    New header component

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ReactNode } from "react";
+import clsx from "clsx";
+import { useTheme } from "next-themes";
+import { getThemeFont } from "@/lib/utils";
+
+
+type PostHeaderProps = {
+  title: string;
+  size: string;
+  className: ReactNode;
+};
+
+export default function PostHeader({
+  title,
+  size,
+  className,
+}: PostHeaderProps) {
+
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <p className={clsx(getThemeFont(theme), `animate-in text-${size} ${className}`)}>
+        {title}
+      </p>
+    </>
+  );
+}
