9e7b68c450a5f5ea71492131ecbb4c2853e0b86f
sellerscrisp <sellershcrisp@gmail.com>
2024-01-05 16:16:44 -0500

    Refactor 404 page layout and content

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,23 @@
+import { DrawerContent, DrawerTrigger } from "@/components/ui/Drawer";
 import Link from "@/components/ui/Link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "404 | S.H. Crisp",
   description:
-    "Uh oh! This page does not exist",
+    "This page does not exist",
 };
 
 const Custom404 = (): JSX.Element => (
-  <div className="flex flex-col gap-2">
-    <h1>404 - Page not found</h1>
-    <p className="text-secondary">
-      Uh oh! This page does not exists, maybe you clicked an old link or
-      misspelled. Please try again…
+  <div className="flex flex-col">
+    <h1 className="text-8xl">ERROR 404</h1>
+    <h2 className="text-xl">Page not found</h2>
+    <p className="text-secondary mt-3">
+      This page does not exist. Please return home or check the URL.
     </p>
     <div className="h-2" />
     <Link href="/" underline>
-      Return home
+      Home
     </Link>
   </div>
 );
