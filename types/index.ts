87b7e583249b95bcc8e2732305e8afadbb690015
sellerscrisp <sellershcrisp@gmail.com>
2024-06-22 13:01:22 -0400

    Update package.json and pnpm-lock.yaml

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,4 @@
 import { type User } from "@prisma/client";
-import { z } from "zod";
 
 export type CurrentUser = {
   id: string;
@@ -14,35 +13,11 @@ export interface payload {
   picture?: string;
 }
 
-export const settingsSchema = z.object({
-  picture: z.string().url(),
-  name: z
-    .string({
-      required_error: "Please type your name.",
-    })
-    .min(3, {
-      message: "Name must be at least 3 characters.",
-    })
-    .max(50, {
-      message: "Name must be at most 50 characters.",
-    }),
-  email: z.string().email(),
-  shortBio: z.string().optional(),
-});
-
-export type SettingsValues = z.infer<typeof settingsSchema>;
-
 export type SubscriptionPlan = {
   name: string;
   description: string;
   stripePriceId: string;
 };
-
-export type UserSubscriptionPlan = SubscriptionPlan &
-  Pick<User, "stripeCustomerId" | "stripeSubscriptionId"> & {
-    stripeCurrentPeriodEnd: number;
-    isPro: boolean;
-  };
 
 export interface SendWelcomeEmailProps {
   toMail: string;
